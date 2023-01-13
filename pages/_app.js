7d8c44603fc5e03a90c5e3280516b49f858f8724
mykmicheals <mykmicheals@gmail.com>
2023-01-13 19:19:12 +0100

    Made header global

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { Provider } from 'react-redux'
 import '../styles/globals.css'
 import store from '../src/store/index';
+import Header from '../src/components/Header'
 
 function MyApp({ Component, pageProps }) {
   return <Provider store={store}>
+    <Header />
     <Component {...pageProps} />
   </Provider>
 
--- src/screens/Homescreen.tsx
+++ src/screens/Homescreen.tsx
@@ -1,23 +1,14 @@
 import React from 'react'
-import Header from '../components/Header'
 import Banner from '../components/Banner';
 import ShopSection from '../components/ShopSection';
-import Card4 from '../components/Card4';
 import Cart from '../components/Cart';
-import { useSelector } from 'react-redux';
-import Slider from '../components/Caraosel';
+
 
 
 function Homescreen() {
 
-    // const products = useSelector((state) => state.products.products)
-
-
     return (
         <div className='relative'>
-            <Header />
-            
-
             <Banner />
             <ShopSection />
             <Cart />
