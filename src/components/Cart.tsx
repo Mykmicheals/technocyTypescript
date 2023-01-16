9769f8372abd06f3dd29ddd10a5a7cadf70b61d9
mykmicheals <mykmicheals@gmail.com>
2023-01-16 15:57:19 +0100

    added .env

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import Speaker from '../../public/images/speaker1.jpg'
 import { cartActions, cartStore } from '../store/index';
 import { serverHost } from './Card4';
+import { PaystackButton } from 'react-paystack'
 
 
 function Cart() {
@@ -14,14 +15,29 @@ function Cart() {
 
 
 
- //   const cart = useSelector((state) => state.cart)
+    //   const cart = useSelector((state) => state.cart)
     const cart = useSelector(cartStore)
 
+    const config = {
+        reference: (new Date()).getTime().toString(),
+        email: "user@example.com",
+        amount: 20000,
+        publicKey: 'pk_test_dd72b6820442e9ed1ecfd39205ef69cd773adcca',
+    }
+
+    const componentProps = {
+
+        ...config,
+        text: "Pay Now",
+        onSuccess: () =>
+            alert("Thanks for doing business with us! Come back soon!!"),
+        onClose: () => alert("Are you sure you want to go back"),
+    }
 
     return (
         <Fragment>
             {cart.showCart && <div>
-               
+
                 <div onClick={() => dispatch(cartActions.hideCart())} className='c-ov w-3/4 divide-y divide-slate-200  backdrop-brightness-50 h-screen fixed top-0 left-0 z-50'></div>
 
                 <div className='c-ov fixed z-10 top-0 right-0 bg-white  h-3/4 w-1/4 p-10 border-b overflow-x-auto'>
@@ -50,7 +66,12 @@ function Cart() {
                     </div>
                     <div className=''>
                         <p className='flex justify-center items-center mt-5 px-4 py-3 rounded font-bold text-sm bg-gray-100 w-4/5 mx-10 cursor-pointer hover:bg-gray-200'>VIEW CART</p>
-                        <p className='flex justify-center items-center mt-2 px-4 py-3 rounded text-white font-bold text-sm bg-red-400 w-4/5 mx-10 hover:bg-red-500'>CHECKOUT</p>
+                        <p className='flex justify-center items-center mt-2 px-4 py-3 rounded text-white font-bold text-sm bg-red-400 w-4/5 mx-10 hover:bg-red-500'>
+                            Checkout
+                        </p>
+
+                        <PaystackButton className="btn" {...componentProps} />
+
                     </div>
 
 
